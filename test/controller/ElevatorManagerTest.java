2808e6f2f472adaa6d7dc40ef272b58c7f7a91d9
Ilias <iliaskomp@gmail.com>
2017-01-12 18:37:44 +0100

    Almost finished ElevatorManagerTest except RemoteExceptions. Now elevators = null before being initiated for testing purposes

--- test/controller/ElevatorManagerTest.java
+++ test/controller/ElevatorManagerTest.java
@@ -2,6 +2,10 @@ package controller;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+import java.rmi.ConnectException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import org.junit.Before;
@@ -15,8 +19,10 @@ import test.DummyElevator;
 import ui.SwingUserInterface;
 
 public class ElevatorManagerTest {
-	ElevatorManager manager;
-	IElevator controller;
+	
+	private static final int MAX_REMOTE_EXCEPTIONS = 5;
+	private ElevatorManager manager;
+	private IElevator controller;
 //	private UserInterface ui;
 
 	@Rule
@@ -40,19 +46,67 @@ public class ElevatorManagerTest {
 		manager.updateElevators();		
 		assertEquals(controller.getFloorNum(), manager.getFloors().size());
 	}
-	
+
 	@Test
 	public void testSetTargetToInvalidFloor() {
 		thrown.expect(NullPointerException.class);
-		manager.setTargetFloor(manager.getElevators().get(0), manager.getFloors().size() + 1);
-		
+		manager.setTargetFloor(manager.getElevators().get(0), manager.getFloors().size() + 1);		
+	}		
+	
+	@Test
+	public void testDefaultDirectionUncommited() {
+		manager.updateElevators();
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, manager.getElevators().get(0).getCommitedDirection());
+	}		
+	
+	@Test
+	public void testElevatorDirectionUp() throws RemoteException {
+		manager.updateElevators();
+		manager.setTargetFloor(manager.getElevators().get(0), 5);
+		manager.updateElevators();
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UP, manager.getElevators().get(0).getCommitedDirection());
 	}
 	
 	@Test
-	public void testExceptionThrownCallingInvalidElevator() {
-		
+	public void testElevatorDirectionDown() throws RemoteException {
+		manager.updateElevators();
+		manager.setTargetFloor(manager.getElevators().get(0), 5);
+		manager.updateElevators();
+		manager.setTargetFloor(manager.getElevators().get(0), 0);
+		manager.updateElevators();
+		assertEquals(IElevator.ELEVATOR_DIRECTION_DOWN, manager.getElevators().get(0).getCommitedDirection());
+	}
+//	Exceptions
+	@Test
+	public void testSetInvalidControllerUrl() throws MalformedURLException, RemoteException, NotBoundException {
+		thrown.expect(RemoteException.class);
+		controller = (IElevator) Naming.lookup("rmi://localhost/WrongUrl");
 	}
 	
-	// throw 5 exceptions
-		
+//	public void testSetTargetFloorThrowsException() {
+//	}
+	
+//	@Test
+//	public void testElevatorsNullWhenMaxRemoteExceptionsThrown() throws RemoteException {		
+//		int exceptionsCatched = 0;
+//
+//		while (exceptionsCatched <= MAX_REMOTE_EXCEPTIONS) {
+//			try {
+//				throw new RemoteException();
+//			} catch (RemoteException e){
+//				exceptionsCatched++;
+//			}
+//		}
+//		
+//		assertEquals(null, manager.getElevators());
+//		manager.updateElevators();
+//		// not working!
+//		// assertEquals(null, manager.getElevators());
+//	}
+	
+	
+	
+	
+	
+	
 }
