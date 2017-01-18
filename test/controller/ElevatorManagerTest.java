0585d25a792254a89e4f0e92893e6dcbec97b5be
Karkus Viktor <karkus.viktor@gmail.com>
2017-01-18 18:35:41 +0100

    Tests added and fixed in ElevatorManagerTest Easymock added DummyElevator fixed

--- test/controller/ElevatorManagerTest.java
+++ test/controller/ElevatorManagerTest.java
@@ -1,112 +1,86 @@
 package controller;
 
-import static org.junit.Assert.*;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
-import java.net.MalformedURLException;
-import java.rmi.ConnectException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
+import org.easymock.EasyMockRule;
+import org.easymock.Mock;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import model.Elevator;
 import sqelevator.IElevator;
-import test.DummyElevatorSimulator;
-import ui.SwingUserInterface;
+import test.DummyElevator;
+import ui.UIInterface;
 
 public class ElevatorManagerTest {
-	
-	private static final int MAX_REMOTE_EXCEPTIONS = 5;
+
 	private ElevatorManager manager;
 	private IElevator controller;
-//	private UserInterface ui;
+	@Rule
+	public EasyMockRule rule = new EasyMockRule(this);
+	@Mock
+	private UIInterface uiMock;
 
 	@Rule
 	public final ExpectedException thrown = ExpectedException.none();
-	
+
 	@Before
 	public void setUp() {
-		controller = new DummyElevatorSimulator();
+		controller = new DummyElevator();
 		manager = new ElevatorManager(controller);
-		
+		manager.updateElevators();
 	}
-		
+
 	@Test
-	public void testElevatorsSize() throws RemoteException {		
-		manager.updateElevators();		
+	public void testElevatorsSize() throws RemoteException {
 		assertEquals(controller.getElevatorNum(), manager.getElevators().size());
 	}
-	
+
 	@Test
-	public void testFloorsSize() throws RemoteException {		
-		manager.updateElevators();		
+	public void testFloorsSize() throws RemoteException {
 		assertEquals(controller.getFloorNum(), manager.getFloors().size());
 	}
 
 	@Test
-	public void testSetTargetToInvalidFloor() {
-		thrown.expect(NullPointerException.class);
-		manager.setTargetFloor(manager.getElevators().get(0), manager.getFloors().size() + 1);		
-	}		
-	
-	@Test
-	public void testDefaultDirectionUncommited() {
+	public void testElevatorCommitedDirection() {
 		manager.updateElevators();
 		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, manager.getElevators().get(0).getCommitedDirection());
-	}		
-	
-	@Test
-	public void testElevatorDirectionUp() throws RemoteException {
-		manager.updateElevators();
 		manager.setTargetFloor(manager.getElevators().get(0), 5);
 		manager.updateElevators();
 		assertEquals(IElevator.ELEVATOR_DIRECTION_UP, manager.getElevators().get(0).getCommitedDirection());
-	}
-	
-	@Test
-	public void testElevatorDirectionDown() throws RemoteException {
-		manager.updateElevators();
-		manager.setTargetFloor(manager.getElevators().get(0), 5);
-		manager.updateElevators();
 		manager.setTargetFloor(manager.getElevators().get(0), 0);
 		manager.updateElevators();
 		assertEquals(IElevator.ELEVATOR_DIRECTION_DOWN, manager.getElevators().get(0).getCommitedDirection());
 	}
-//	Exceptions
+
+	// Errors
+
 	@Test
-	public void testSetInvalidControllerUrl() throws MalformedURLException, RemoteException, NotBoundException {
-		thrown.expect(RemoteException.class);
-		controller = (IElevator) Naming.lookup("rmi://localhost/WrongUrl");
+	public void testSetTargetToInvalidFloor() {
+		uiMock.showError("Could not send the command to the elevator. Please try again.");
+		replay(uiMock);
+		manager.setUI(uiMock);
+		manager.setTargetFloor(manager.getElevators().get(0), -1);
+		verify(uiMock);
 	}
-	
-//	public void testSetTargetFloorThrowsException() {
-//	}
-	
-//	@Test
-//	public void testElevatorsNullWhenMaxRemoteExceptionsThrown() throws RemoteException {		
-//		int exceptionsCatched = 0;
-//
-//		while (exceptionsCatched <= MAX_REMOTE_EXCEPTIONS) {
-//			try {
-//				throw new RemoteException();
-//			} catch (RemoteException e){
-//				exceptionsCatched++;
-//			}
-//		}
-//		
-//		assertEquals(null, manager.getElevators());
-//		manager.updateElevators();
-//		// not working!
-//		// assertEquals(null, manager.getElevators());
-//	}
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void testMultipleUpdateError() throws RemoteException {
+		uiMock.showError("Connection lost to the elevator. Please restart the application.");
+		replay(uiMock);
+		manager.setUI(uiMock);
+		manager.getElevators().get(0).setElevatorNumber(1);
+		int exceptionsCatched = 0;
+		while (exceptionsCatched <= ElevatorManager.MAX_REMOTE_EXCEPTIONS + 1) {
+			manager.updateElevators();
+			exceptionsCatched++;
+		}
+		verify(uiMock);
+	}
+
 }
--- test/test/DummyElevator.java
+++ test/test/DummyElevator.java
@@ -4,139 +4,115 @@ import java.rmi.RemoteException;
 
 import sqelevator.IElevator;
 
-public class DummyElevator implements IElevator{
-	private static final int POSITION = 10;
-	private static final int SPEED = 10;
-	private static final int WEIGHT = 10;
-	private static final int DOOR_STATUS = 1;
-	private static final int COMMITED_DIRECTION = 1;
-	private static final int COMMITED_DIRECTION_SET = 2;
+public class DummyElevator implements IElevator {
 
-	private static final int FLOOR_HEIGHT = 6;
-	private static final int TARGET_FLOOR = 11;
 	private int direction, target;
-	
+
 	public DummyElevator() {
-		direction = COMMITED_DIRECTION;
+		direction = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
 		target = 0;
 	}
-	
+
 	@Override
 	public int getCommittedDirection(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
 		return direction;
 	}
 
 	@Override
 	public int getElevatorAccel(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public boolean getElevatorButton(int elevatorNumber, int floor) throws RemoteException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public int getElevatorDoorStatus(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
-		return DOOR_STATUS;
+		return IElevator.ELEVATOR_DOORS_CLOSED;
 	}
 
 	@Override
 	public int getElevatorFloor(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int getElevatorNum() throws RemoteException {
-		// TODO Auto-generated method stub
 		return 1;
 	}
 
 	@Override
 	public int getElevatorPosition(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
-		return POSITION;
+		if(elevatorNumber>0) throw new RemoteException();
+		return 0;
 	}
 
 	@Override
 	public int getElevatorSpeed(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
-		return SPEED;
+		return 0;
 	}
 
 	@Override
 	public int getElevatorWeight(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
-		return WEIGHT;
+		return 0;
 	}
 
 	@Override
 	public int getElevatorCapacity(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public boolean getFloorButtonDown(int floor) throws RemoteException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean getFloorButtonUp(int floor) throws RemoteException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public int getFloorHeight() throws RemoteException {
-		// TODO Auto-generated method stub
-		return FLOOR_HEIGHT;
+		return 0;
 	}
 
 	@Override
 	public int getFloorNum() throws RemoteException {
-		// TODO Auto-generated method stub
-		return 14;
+		return 10;
 	}
 
 	@Override
 	public boolean getServicesFloors(int elevatorNumber, int floor) throws RemoteException {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public int getTarget(int elevatorNumber) throws RemoteException {
-		// TODO Auto-generated method stub
-		return 0;
+		return target;
 	}
 
 	@Override
 	public void setCommittedDirection(int elevatorNumber, int direction) throws RemoteException {
-		direction = COMMITED_DIRECTION_SET;
+		this.direction = direction;
 	}
 
 	@Override
 	public void setServicesFloors(int elevatorNumber, int floor, boolean service) throws RemoteException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void setTarget(int elevatorNumber, int target) throws RemoteException {
-		this.target = target;		
+		if (target < 0)
+			throw new RemoteException();
+		this.target = target;
 	}
 
 	@Override
 	public long getClockTick() throws RemoteException {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 }
