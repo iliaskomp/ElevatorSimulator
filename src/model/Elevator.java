8c13c42b8f71d13d7dafd8f543e8818eaa01ee03
Ilias <iliaskomp@gmail.com>
2017-01-19 20:36:00 +0100

    Generated javadoc for model classes Elevator and Floor

--- src/model/Elevator.java
+++ src/model/Elevator.java
@@ -1,98 +1,228 @@
 package model;
 
+/**
+ * The Class Elevator.
+ * Model of the elevator.
+ * @author Ilias Komp
+ */
 public class Elevator {
+	
+	/** The elevator number. */
 	private int elevatorNumber;
+	
+	/** The position. */
 	private int position; // position in feet
+	
+	/** The position nearest floor. */
 	private int positionNearestFloor; // position in relation to nearest floor
+	
+	/** The speed. */
 	private int speed;
+	
+	/** The weight. */
 	private int weight;
+	
+	/** The door status. */
 	private int doorStatus;
+	
+	/** The manual mode. */
 	private boolean manualMode;
+	
+	/** The name. */
 	private String name;
+	
+	/** The commited direction. */
 	private int commitedDirection;
+	
+	/** The target floor. */
 	private int targetFloor;
 
+	/**
+	 * Instantiates a new elevator.
+	 *
+	 * @param elevatorNumber the elevator number
+	 */
 	public Elevator(int elevatorNumber) {
 		this.elevatorNumber = elevatorNumber;
 	}
 
+	/**
+	 * Gets the elevator number.
+	 *
+	 * @return the elevator number
+	 */
 	// Getters/Setters
 	public int getElevatorNumber() {
 		return elevatorNumber;
 	}
 
+	/**
+	 * Sets the elevator number.
+	 *
+	 * @param elevatorNumber the new elevator number
+	 */
 	public void setElevatorNumber(int elevatorNumber) {
 		this.elevatorNumber = elevatorNumber;
 	}
 
+	/**
+	 * Gets the door status.
+	 *
+	 * @return the door status
+	 */
 	public int getDoorStatus() {
 		return doorStatus;
 	}
 
+	/**
+	 * Sets the door status.
+	 *
+	 * @param doorStatus the new door status
+	 */
 	public void setDoorStatus(int doorStatus) {
 		this.doorStatus = doorStatus;
 	}
 
+	/**
+	 * Gets the position.
+	 *
+	 * @return the position
+	 */
 	public int getPosition() {
 		return position;
 	}
 
+	/**
+	 * Sets the position.
+	 *
+	 * @param position the new position
+	 */
 	public void setPosition(int position) {
 		this.position = position;
 	}
 
+	/**
+	 * Gets the speed.
+	 *
+	 * @return the speed
+	 */
 	public int getSpeed() {
 		return speed;
 	}
 
+	/**
+	 * Sets the speed.
+	 *
+	 * @param speed the new speed
+	 */
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
 
+	/**
+	 * Checks if is manual mode.
+	 *
+	 * @return true, if is manual mode
+	 */
 	public boolean isManualMode() {
 		return manualMode;
 	}
 
+	/**
+	 * Sets the manual mode.
+	 *
+	 * @param manualMode the new manual mode
+	 */
 	public void setManualMode(boolean manualMode) {
 		this.manualMode = manualMode;
 	}
 
+	/**
+	 * Gets the nearest floor.
+	 *
+	 * @return the nearest floor
+	 */
 	public int getNearestFloor() {
 		return positionNearestFloor;
 	}
 
+	/**
+	 * Sets the nearest floor.
+	 *
+	 * @param nearestFloor the new nearest floor
+	 */
 	public void setNearestFloor(int nearestFloor) {
 		this.positionNearestFloor = nearestFloor;
 	}
 
+	/**
+	 * Gets the weight.
+	 *
+	 * @return the weight
+	 */
 	public int getWeight() {
 		return weight;
 	}
 
+	/**
+	 * Sets the weight.
+	 *
+	 * @param weight the new weight
+	 */
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets the commited direction.
+	 *
+	 * @return the commited direction
+	 */
 	public int getCommitedDirection() {
 		return commitedDirection;
 	}
 
+	/**
+	 * Sets the commited direction.
+	 *
+	 * @param commitedDirection the new commited direction
+	 */
 	public void setCommitedDirection(int commitedDirection) {
 		this.commitedDirection = commitedDirection;
 	}
 
+	/**
+	 * Gets the target floor.
+	 *
+	 * @return the target floor
+	 */
 	public int getTargetFloor() {
 		return targetFloor;
 	}
 
+	/**
+	 * Sets the target floor.
+	 *
+	 * @param targetFloor the new target floor
+	 */
 	public void setTargetFloor(int targetFloor) {
 		this.targetFloor = targetFloor;
 	}
--- src/model/Floor.java
+++ src/model/Floor.java
@@ -1,43 +1,102 @@
 package model;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Floor.
+ * Model of Floor.
+ * @author Ilias Komp
+ */
 public class Floor {
+	
+	/** The up call. */
 	private boolean upCall;
+	
+	/** The down call. */
 	private boolean downCall;
+	
+	/** The stop request. */
 	private boolean stopRequest;
+	
+	/** The floor. */
 	private int floor;
 	
+	/**
+	 * Instantiates a new floor.
+	 *
+	 * @param floor (number of floor)
+	 */
 	public Floor(int floor) {
 		this.setFloor(floor);
 	}
 
+	/**
+	 * Checks if is up call.
+	 *
+	 * @return true, if is up call
+	 */
 	public boolean isUpCall() {
 		return upCall;
 	}
 
+	/**
+	 * Sets the up call.
+	 *
+	 * @param upCall the new up call
+	 */
 	public void setUpCall(boolean upCall) {
 		this.upCall = upCall;
 	}
 
+	/**
+	 * Checks if is down call.
+	 *
+	 * @return true, if is down call
+	 */
 	public boolean isDownCall() {
 		return downCall;
 	}
 
+	/**
+	 * Sets the down call.
+	 *
+	 * @param downCall the new down call
+	 */
 	public void setDownCall(boolean downCall) {
 		this.downCall = downCall;
 	}
 
+	/**
+	 * Checks if is stop request.
+	 *
+	 * @return true, if is stop request
+	 */
 	public boolean isStopRequest() {
 		return stopRequest;
 	}
 
+	/**
+	 * Sets the stop request.
+	 *
+	 * @param stopRequest the new stop request
+	 */
 	public void setStopRequest(boolean stopRequest) {
 		this.stopRequest = stopRequest;
 	}
 
+	/**
+	 * Gets the floor.
+	 *
+	 * @return the floor
+	 */
 	public int getFloor() {
 		return floor;
 	}
 
+	/**
+	 * Sets the floor.
+	 *
+	 * @param floor the new floor
+	 */
 	public void setFloor(int floor) {
 		this.floor = floor;
 	}
